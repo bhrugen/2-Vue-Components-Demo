399627c63d5f34b7363b697599360757aca1058b
Bhrugen Patel <bhrugen1990@gmail.com>
2025-02-19 15:14:22 -0500

    Section 11 - Passing props

---
+++ vue-components-demo/src/components/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-info rounded p-2 m-2">
+    <h3>Name :{{ name }}</h3>
+    <p>Email : {{ email }}</p>
+    <p>Phone : {{ phone }}</p>
+  </div>
+</template>
+<script setup>
+import { ref, defineProps } from "vue";
+const email = ref("hello@dotnetmastery.com");
+const props = defineProps(["name", "phone"]);
+</script>
