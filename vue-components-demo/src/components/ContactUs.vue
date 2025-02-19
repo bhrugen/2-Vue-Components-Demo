526e8265cc7fb53f22d2ec4dc414911048a35521
Bhrugen Patel <bhrugen1990@gmail.com>
2025-02-19 14:14:03 -0500

    Section 10 - Create another component

--- vue-components-demo/src/components/ContactUs.vue
+++ vue-components-demo/src/components/ContactUs.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="">
     <h1>Contact Us</h1>
-    <p>Please send an email to hello@dotnetmastery.com</p>
+    <p>Please send an email to {{ email }}</p>
   </div>
 </template>
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const email = ref("hello@dotnetmastery.com");
+    return { email };
+  },
+};
+</script>
