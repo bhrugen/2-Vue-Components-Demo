75c2264f4b95701bf8ba3b9fcd0374c49154140a
Bhrugen Patel <bhrugen1990@gmail.com>
2025-02-19 15:54:36 -0500

    Section 11 - List of contacts

--- vue-components-demo/src/components/Contact.vue
+++ vue-components-demo/src/components/Contact.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="bg-info rounded p-4 m-2">
-    <h3>Name :{{ name }}</h3>
-    <p>Email : {{ email }}</p>
-    <p>Phone : {{ phone }}</p>
-    <p class="float-end small" v-if="ownername != ''">
-      *this contact info belongs to {{ ownername }}
-    </p>
+  <div class="bg-info rounded p-1 m-1">
+    <div class="row">
+      <div class="col-12">
+        <h3>Name :{{ name }}</h3>
+        <p>Email : {{ email }}</p>
+        <p>Phone : {{ phone }}</p>
+        <p class="float-end small" v-if="ownername != ''">
+          *this contact info belongs to {{ ownername }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
@@ -14,5 +18,6 @@ const props = defineProps({
   phone: Number,
   ownername: String,
   email: { type: String, required: false, default: "-n/a-" },
+  isFavorite: Boolean,
 });
 </script>
