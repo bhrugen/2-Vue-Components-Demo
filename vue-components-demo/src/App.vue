0feed68f4785ccc41445ea936b71619ebc3c22fc
Bhrugen Patel <bhrugen1990@gmail.com>
2025-02-19 13:22:12 -0500

    Section 9 - vue in action

--- vue-components-demo/src/App.vue
+++ vue-components-demo/src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>Hello world</div>
+  <div>{{ message }}</div>
 </template>
+
+<script>
+export default {
+  setup() {
+    const message = "Hello Vue";
+    return { message };
+  },
+};
+</script>
+
+<style></style>
