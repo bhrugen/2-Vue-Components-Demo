aa53201ce2b0b03015fc7b178713072e9b55c266
Bhrugen Patel <bhrugen1990@gmail.com>
2025-02-21 12:15:31 -0500

    Section 13 - Lifecycle Demo

--- vue-components-demo/src/components/ButtonCounter.vue
+++ vue-components-demo/src/components/ButtonCounter.vue
@@ -2,6 +2,33 @@
   <button @click="count++">Clicked {{ count }} times</button>
 </template>
 <script setup>
-import { ref } from "vue";
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
+
+onBeforeMount(() => {
+  console.log("onBeforeMount - ButtonCounter.vue");
+});
+onMounted(() => {
+  console.log("onMounted- ButtonCounter.vue");
+});
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate- ButtonCounter.vue");
+});
+onUpdated(() => {
+  console.log("onUpdated- ButtonCounter.vue");
+});
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount- ButtonCounter.vue");
+});
+onUnmounted(() => {
+  console.log("onUnmounted- ButtonCounter.vue");
+});
 const count = ref(0);
 </script>
