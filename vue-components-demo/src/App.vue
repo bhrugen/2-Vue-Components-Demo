7e890729b1a19d9a739a4493efb842aefba6f3ea
Bhrugen Patel <bhrugen1990@gmail.com>
2025-02-21 10:10:18 -0500

    Section 13 - Cleanup

--- vue-components-demo/src/App.vue
+++ vue-components-demo/src/App.vue
@@ -1,42 +1,7 @@
 <template>
-  <div class="bg-black text pt-3" :style="{ height: '100vh' }">
-    <h1 class="text-center text-success">ContactOPedia</h1>
-    <div class="container">
-      <button
-        class="btn btn-primary text-black m-2"
-        @click="newVersion = !newVersion"
-      >
-        Toggle Componenet
-      </button>
-      <br />
-      <button
-        class="btn btn-primary text-black m-2"
-        @click="newVersion = false"
-      >
-        Lucky Number V1
-      </button>
-      <button class="btn btn-primary text-black m-2" @click="newVersion = true">
-        Lucky Number V2
-      </button>
-      <br />
-      <br />
-      <keep-alive :include="['LuckyNumber', 'LuckyNumberV2']">
-        <component :is="currentComponent" />
-      </keep-alive>
-    </div>
-  </div>
+  <LuckyNumberParentComponent></LuckyNumberParentComponent>
 </template>
 
 <script setup>
-import { ref, reactive, provide, compile, computed } from "vue";
-import LuckyNumber from "./components/LuckyNumber.vue";
-import LuckyNumberV2 from "./components/LuckyNumberV2.vue";
-
-const newVersion = ref(true);
-
-const currentComponent = computed(() => {
-  return newVersion.value ? LuckyNumberV2 : LuckyNumber;
-});
+import LuckyNumberParentComponent from "./components/LuckyNumberParentComponent.vue";
 </script>
-
-<style></style>
