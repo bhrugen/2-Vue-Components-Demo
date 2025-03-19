3987b5178aeb98af87f922ed799205595293889e
Bhrugen Patel <bhrugen1990@gmail.com>
2025-03-18 19:21:35 -0500

    Section 13 - Props in Composibles

--- vue-components-demo/src/App.vue
+++ vue-components-demo/src/App.vue
@@ -10,5 +10,5 @@
 <script setup>
 import { useCounter } from "./composibles/useCounter";
 import ButtonCounter from "./components/ButtonCounter.vue";
-const { count, increment, decrement } = useCounter();
+const { count, increment, decrement } = useCounter(100, 10);
 </script>
